fb950310dc7c221af06ed61201a2c7e61e079902
ycabrera90 <68780363+ycabrera90@users.noreply.github.com>
2020-08-17 21:36:44 -0300

    1

--- login.js
+++ login.js
@@ -1,4 +1,9 @@
-
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+  console.log('User signed out.');
+  });
+}
 
 
 
@@ -18,7 +23,7 @@ function onSignIn(googleUser) {
     var id_token = googleUser.getAuthResponse().id_token;
     console.log("ID Token: " + id_token);
     
-    window.location="backend.html";
+    //window.location="backend.html";
   }
 
 
